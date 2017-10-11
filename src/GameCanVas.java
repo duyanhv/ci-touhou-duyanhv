49c8d8d19439d137e6b870d30ded6691405cc9ab
Vu Duy Anh <duyanh@MacBook-Pro-cua-Vu.local>
2017-10-11 19:43:23 +0700

    added assets

---
+++ src/GameCanVas.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by duyanh on 10/11/17.
+ */
+public class GameCanVas extends JPanel{
+    public GameCanVas(){
+
+    }
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -5,9 +5,14 @@ import java.awt.*;
  * Created by duyanh on 10/11/17.
  */
 public class GameWindow extends JFrame{
+    GameCanVas canvas;
     public GameWindow(){
         this.setSize(800,600);
         this.setVisible(true);
-        
+//        this.setBackground(Color.BLACK);
+
+        this.canvas = new GameCanVas();
+        this.setContentPane(this.canvas);
+
     }
 }
